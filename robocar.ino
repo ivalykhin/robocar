9e9e3328f03663ee643d8127463ff505711700e1
ivalykhin <ivalykhin@gmail.com>
2016-04-19 21:50:57 +0600

    add temp and humidity monitoring

--- robocar.ino
+++ robocar.ino
@@ -1,4 +1,5 @@
 #include <AFMotor.h>
+#include <DHT.h>
 
 
 // Инициируем моторы
@@ -6,12 +7,19 @@ AF_DCMotor motor1(4);
 AF_DCMotor motor2(3);
 
 // Пины
+#define TEMP_PIN 13 // номер пина, к которому подсоединен датчик температуры
+
+// Таймеры
+
 
 // Инициируем датчики
+DHT dht(TEMP_PIN, DHT22);
 
 // Инициируем переменные
-float vcc = 0.0;
-char controlChar;
+float vcc = 0.0;  // Входящее напряжение
+char controlChar;  // Символ контроля
+long previousMillis = 0; // время, когда действие выполнялось последний раз
+long interval = 1000; // период ожидания следующего действия
 
 float readVCC() {
   ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
@@ -23,7 +31,6 @@ float readVCC() {
   result = (1.1 * 1023.0) / result; // Результат Vcc в милливольтах
   return result;
 }
-
 
 void forward() {
   motor1.run(RELEASE);
@@ -55,6 +62,8 @@ void right() {
 
 void setup() {
   Serial.begin(9600);
+  dht.begin();
+
   // Задаем максимальную скорость вращения моторов (аналог работы PWM)
   motor1.setSpeed(255);
   motor1.run(RELEASE);
@@ -63,9 +72,31 @@ void setup() {
 }
 
 void loop() {
-  //vcc = readVCC();
-  //Serial.print("VCC: ");
-  //Serial.println(vcc);
+  unsigned long currentMillis = millis(); // текущее время в миллисекундах
+
+  if (currentMillis - previousMillis > interval) {
+    // сохраняем последний момент, когда выполнялось действие
+    previousMillis = currentMillis;
+    vcc = readVCC();
+    Serial.print("VCC: ");
+    Serial.println(vcc);
+
+    //Считываем влажность
+    float h = dht.readHumidity();
+    // Считываем температуру
+    float t = dht.readTemperature();
+    // Проверка удачно прошло ли считывание.
+    if (isnan(h) || isnan(t)) {
+      Serial.println("Не удается считать показания");
+    } else {
+      Serial.print("Humidity: ");
+      Serial.print(h);
+      Serial.print("%, Temperature: ");
+      Serial.print(t);
+      Serial.println("*C");
+    }
+  }
+
   if (Serial.available())
   {
     Serial.print("Command is ");
